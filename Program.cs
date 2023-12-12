bd17d4cb9c67d421118c54d4b70bf9bcfedf4170
Alena <alenakrylova69@gmail.com>
2023-12-12 15:35:34 +0300

    Создана ветка 'task2' с задачей 2

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
-﻿// Задача 1: Задайте одномерный массив из 10 целых
-// чисел от 1 до 100. Найдите количество элементов
-// массива, значения которых лежат в отрезке [20,90].
+﻿// Задача 2: Задайте массив на 10 целых чисел.
+// Напишите программу, которая определяет
+// количество чётных чисел в массиве.
 
 int[] array = new int[10];
 for ( int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(1,101);
+    array[i] = new Random().Next(0,10);
     Console.Write(array[i] + " ");
 }
 int count = 0;
 for (int i = 0; i < array.Length; i++)
 {  
-    if (array[i] >=20 && array[i] <=90)
+    if (array[i]%2 == 0)
     {
         count++;
     }   
 }
     Console.WriteLine();
-    Console.WriteLine(count +" - количество элементов от 20 до 90");
+    Console.WriteLine(count +" - количество четных чисел");
